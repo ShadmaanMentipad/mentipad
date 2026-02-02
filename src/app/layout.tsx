3148ac20c49293b5c3cc0a3c2d2cbb2c5a45a3a5
Dharaniliv <dharaniprasath8015@gmail.com>
2026-02-02 16:00:55 +0530

    google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,34 +3,18 @@ import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Providers from "./providers";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({ subsets: ["latin"], weight: ["600", "700"] });
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>
-          <Navbar />
-          {children}
-          <Footer />
-        </Providers>
-      </body>
-    </html>
-  );
-}
 
 export const metadata = {
   title: "Mentipad – Find Trusted Mentors for Competitive Exams & Careers",
   description:
     "Mentipad connects students and professionals with top mentors for competitive exams, academic success, and career growth. Book 1:1 sessions and unlock your potential.",
-     icons: {
-    icon: "/favicon.png",},
+  icons: {
+    icon: "/favicon.png",
+  },
   keywords: [
     "mentorship",
     "online mentors",
@@ -51,3 +35,27 @@ export const metadata = {
   authors: [{ name: "Mentipad Team", url: "https://mentipad.com" }],
   creator: "Mentipad",
 };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="google-site-verification: googlef0435c4567fbff5a.html"
+        />
+      </Head>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          {children}
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
